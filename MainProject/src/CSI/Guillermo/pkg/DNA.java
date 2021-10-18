a2a51350f55a18b9d8fa90027a3708aebc5732e9
CSI-Guillermo-Perez <89141135+CSI-Guillermo-Perez@users.noreply.github.com>
2021-10-18 09:01:34 -0400

    DNA completed
    👍

---
+++ MainProject/src/CSI/Guillermo/pkg/DNA.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package CSI.Guillermo.pkg;
+
+import java.util.Arrays;
+
+/**
+ * @author guillermoperez
+ *
+ */
+public class DNA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+while(true) {
+		int[][] arr = new int[9][9];
+		for(int i = 0; i < arr.length; i++) {
+		    for(int j = 0; j < arr[i].length; j++) {
+		      arr[i][i]=1;
+		      arr[i][arr[i].length - i - 1 ]=1;
+		    }
+		    System.out.println(Arrays.toString(arr[i]));
+		    Thread.sleep(100);
+		}
+	}
+	}
+}
