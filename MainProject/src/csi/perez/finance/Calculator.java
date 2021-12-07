8d32af11b8597f7a2269076a14737777f8dd2c10
CSI-Guillermo-Perez <89141135+CSI-Guillermo-Perez@users.noreply.github.com>
2021-12-06 22:38:40 -0400

    Calculator
    Final job for this semester

--- MainProject/src/csi/perez/finance/Calculator.java
+++ MainProject/src/csi/perez/finance/Calculator.java
@@ -15,14 +15,13 @@ public class Calculator {
 
 		double initial = 2000;
 		double cash = 0;
-		double invested = 0;
 
 		List<Loan> loans = new ArrayList<Loan>();
 
-		loans.add(new Loan("Student Loans", 0, 0.0466, 24));
+		loans.add(new Loan("Student Loans", 0, 0.0466, 30));
 		loans.add(new Loan("Personal Loans", 0, 0.16, 10));
 		loans.add(new Loan("Mortgage Loans", 0, 0.03125, 30));
-		loans.add(new Loan("CreditCard", 0, 0, 99));
+		loans.add(new Loan("CreditCard", 0, 0.29, 70));
 
 		cash += initial;
 
@@ -43,15 +42,16 @@ public class Calculator {
 				double costOfLiving = 14_000;
 
 				cash += salary;
+				cash -= costOfLiving;
 				cash += askForMoney(costOfLiving);
-				cash += askForMoney(tuition);
-				cash -= tuition;
-//				  loans.get(0).balance += tuition; (Wont work)
+				loans.get(0).principal += tuition;
+				loans.get(0).differed = true;
 
 			}
 
 // Work as a starting Nuclear Engineer in a studio apartment
 			else if (age <= 28) {
+				loans.get(0).differed = false;
 				String Location = "Oklahoma";
 				// Salary -10% tax
 				double salary = 57_870 * 0.9;
@@ -117,7 +117,7 @@ public class Calculator {
 //Loans
 			for (Loan loan : loans) {
 				// Carry out 12 payments (monthly)
-				for (int month = 0; month < 12; month++) {
+				for (int month = 0; month < 60; month++) {
 					// If this loan is still due and has not been differed
 					if (loan.getBalance() > 0 && loan.differed == false) {
 						cash -= loan.makePayment();
@@ -125,14 +125,14 @@ public class Calculator {
 				}
 			}
 
-			double debt = 0;
-			for (Loan loan : loans)
-				debt += loan.getBalance();
+
 
 //				Additional logic
 //Car
-			// For college I will not be using a car, I will be walking since I live on campus
-			// Bought a used car at 24 after finishing college and starting work as a noobie Nuclear Engineer
+			// For college I will not be using a car, I will be walking since I live on
+			// campus
+			// Bought a used car at 24 after finishing college and starting work as a noobie
+			// Nuclear Engineer
 			if (age == 24 && cash >= 15_000) {
 				double usedCarPrice = 10_000;
 				cash -= usedCarPrice;
@@ -140,28 +140,28 @@ public class Calculator {
 			}
 
 			// Bought a new car at 32 as a mid-level Nuclear Engineer
-	
+
 			if (age == 32 && cash < 70_000 && cash > 40_000) {
 				double newCarPrice = 64_000;
 				double downPayment = 64_000 * 0.15;
 				double interestRate = 0.05;
-				int repaymentYears = 6;
+				int repaymentYears = 8;
 
 				Loan carLoan = new Loan("Car Loan", (newCarPrice - downPayment), interestRate, repaymentYears);
 				loans.add(carLoan);
 
 				System.out.println("Monthly car Payment: " + carLoan.getMonthlyPayment() + " For a total of "
 						+ carLoan.getTotalPayment());
-			}	
+			}
 			// Car should last me at least more than 15 years so no need to buy another one
+
 //House
 			// Bought a House at 35 as a mid-level Nuclear Engineer
 			if (age == 35 && cash > 220_000) {
 				double housePrice = 205_000;
 				double downPayment = housePrice * 0.15;
 				double interestRate = 0.03;
-				// int repaymentYears = 15;
-				int repaymentYears = 30;
+				int repaymentYears = 25;
 
 				Loan mortgageLoan = new Loan("My first home", (housePrice - downPayment), interestRate, repaymentYears);
 				loans.add(mortgageLoan);
@@ -169,11 +169,19 @@ public class Calculator {
 				System.out.println("House mortgage: " + mortgageLoan.getMonthlyPayment() + " For a total of "
 						+ mortgageLoan.getTotalPayment());
 			}
+			
+			
+			
+			// Calculate Debt Balance
+			double debt = 0;
+			for (Loan loan : loans)
+				debt += loan.getBalance();
 
-			{
-				System.out.println("Balance at age: " + age + " is: " + cash + " with a debt of " + debt + " and "
-						+ invested + " invested.");
-			}
+			double invested = 0;
+
+			System.out.println("Balance at age: " + age + " is: " + cash + " with a debt of " + debt + " and "
+					+ invested + " invested.");
+
 			if (cash < 0) {
 				System.out.println("Out of cash. You Died!");
 				break;
--- MainProject/src/csi/perez/finance/Loan.java
+++ MainProject/src/csi/perez/finance/Loan.java
@@ -8,42 +8,42 @@ package csi.perez.finance;
  *
  */
 public class Loan {
-String name; 
-double principal;
-double annualInterestRate;
-double numberOfYears;
-double paid;
-double balance;
-boolean differed = false;
+	String name;
+	double principal;
+	double annualInterestRate;
+	double numberOfYears;
+	double paid;
+	boolean differed = false;
 
+	public Loan(String name, double principal, double annualInterestRate, int numberOfYears) {
+		super();
+		this.name = name;
+		this.principal = principal;
+		this.annualInterestRate = annualInterestRate;
+		this.numberOfYears = numberOfYears;
 
-
-public Loan(String name, double principal, double annualInterestRate, int numberOfYears) {
-	super();
-	this.name = name;
-	this.principal = principal;
-	this.annualInterestRate = annualInterestRate;
-	this.numberOfYears = numberOfYears;
-	
-}
-
-public double makePayment() {
-	paid += getMonthlyPayment();
-	return getMonthlyPayment();
-}
-	public double getBalance() {
-	return (getTotalPayment() - paid );
 	}
+
+	public double makePayment() {
+		paid += getMonthlyPayment();
+		return getMonthlyPayment();
+	}
+
+	public double getBalance() {
+		return (getTotalPayment() - paid);
+	}
+
 //Find Monthly Payment
-public double getMonthlyPayment() {
-	double monthlyInterestRate = annualInterestRate / 1200;
-	double monthlyPayment = principal * monthlyInterestRate / (1 - 1 / Math.pow
-			(1 + monthlyInterestRate, numberOfYears * 12));
-			return monthlyPayment;
-}
+	public double getMonthlyPayment() {
+		double monthlyInterestRate = annualInterestRate / 1200;
+		double monthlyPayment = principal * monthlyInterestRate
+				/ (1 - 1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
+		return monthlyPayment;
+	}
 
 //Find total payment
-public double getTotalPayment() {
-	return (getMonthlyPayment() * numberOfYears * 12);
-}
+	public double getTotalPayment() {
+		double totalPayment = getMonthlyPayment() * numberOfYears * 12;
+		return totalPayment;
+	}
 }
